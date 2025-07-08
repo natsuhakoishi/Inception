e4bcc5c409e4637c4b61d76a77c4bc6013d5c346
Natsuha <ethanyap8@gmail.com>
2025-07-08 20:02:13 +0800

    D-C

--- srcs/requirements/wordpress/tools/wp_setup.sh
+++ srcs/requirements/wordpress/tools/wp_setup.sh
@@ -31,11 +31,11 @@ then
 	--admin_email=$WP_ADMIN_EMAIL --allow-root
 	echo "Wordpress Core Installed"
 
-	wp user create --path=/var/www/html/wordpress \
-	$WP_USER $WP_EMAIL --user_pass=$WP_PASS \
-	--role=author --allow-root
+	# wp user create --path=/var/www/html/wordpress \
+	# $WP_USER $WP_EMAIL --user_pass=$WP_PASS \
+	# --role=author --allow-root
 
-	wp install theme twentytwentytwo --activate --allow-root
+	wp theme install twentytwentytwo --activate --allow-root
 fi
 
 sed -i 's;listen = /run/php/php7.4-fpm.sock;listen = 9000;' /etc/php/7.4/fpm/pool.d/www.conf
